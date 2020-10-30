45f077fb9b8f5ee16e57564781e4a61adb4c53d1
Walter Chapilliquen <wz.vang@gmail.com>
2020-10-30 16:40:01 -0500

    f

--- free.php
+++ free.php
@@ -3,7 +3,24 @@
     require("./config.php");
 
     echo isset($_REQUEST["controller"]) ? 'Controller: ' . $_REQUEST["controller"] . "<br />" : "";
-    die('<a href="./descargas/bot_free.zip">Descarga<a>');
+    die('
+    
+
+    <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fweb.facebook.com%2FWAI.Technology%2F&width=370&layout=standard&action=like&size=small&share=false&height=35" width="370" height="35" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+        <br>    <a href="./descargas/bot_free.zip">Descarga<a>
+
+        <ul>
+        <li>Abrir extensiones de Chrome<li>
+        <li>Activar modo desarrollador<li>
+        <li>Clic botón Cargar Descomprimida <li>
+        <li>Seleccionar la carpeta bot_free<li>
+        <li>Abrir dragonbound.net<li>
+        <li>Disfrutar<li>
+        </ul>
+
+        <h3>Para adquirir versión Premium Indetectable</h3>
+        <p>Precio S/100 para 2 UserID por 1 mes (BOT PRIVADO). Escribir al inbox del FB o al WhatsApp <a href="https://wa.me/51902425910?text=Me%20interesa%20in%20el%20bot%20que%20vendes">902425910</a></p>
+    ');
     
 
 ?>
