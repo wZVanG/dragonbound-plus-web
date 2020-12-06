68161960ae1c5f429f0918625fb71a32e8acd61e
Walter Chapilliquen <wz.vang@gmail.com>
2020-12-06 13:41:31 -0500

    whsp

--- premium/index.php
+++ premium/index.php
@@ -1,5 +1,5 @@
 <?php 
 
-header("Location: https://www.facebook.com/messages/t/DragonBound.Aimbot");
+header("Location: https://api.whatsapp.com/send?phone=51902425910&text=Estoy%20interesado%20en%20comprar%20el%20Bot%20PREMIUM");
 
 ?>
