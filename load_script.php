802bff8f26136211e5eecea7f13ee8b8d2c89cd9
Walter Chapilliquen <wz.vang@gmail.com>
2020-12-14 13:22:55 -0500

    cors

--- load_script.php
+++ load_script.php
@@ -1,9 +1,10 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+header("Content-Type: application/javascript");
 $method = $_SERVER['REQUEST_METHOD'];
 if($method == "OPTIONS") exit;
 $script = !empty($_GET["script"]) ? $_GET["script"] : null;
